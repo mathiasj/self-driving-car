1962c205a2eb1045b073ea21d9d6a1dc4ed2bd5a
Mathias Johansson <hej@mathiasjohansson.se>
2023-08-02 13:30:28 +0200

    Allow rayCount of 1

--- sensor.js
+++ sensor.js
@@ -1,9 +1,9 @@
 class Sensor {
   constructor(car) {
     this.car = car
-    this.rayCount = 3
-    this.rayLength = 100
-    this.raySpread = Math.PI / 4 // radian equivalent of 45 degrees.
+    this.rayCount = 4
+    this.rayLength = 150
+    this.raySpread = Math.PI / 2 // radian equivalent of 90 degrees.
 
     this.rays = []
   }
@@ -12,8 +12,11 @@ class Sensor {
     this.rays = []
     for (let i = 0; i < this.rayCount; i++) {
       const rayAngle =
-        lerp(this.raySpread / 2, -this.raySpread / 2, i / (this.rayCount - 1)) +
-        this.car.angle
+        lerp(
+          this.raySpread / 2,
+          -this.raySpread / 2,
+          this.rayCount == 1 ? 0.5 : i / (this.rayCount - 1)
+        ) + this.car.angle
 
       const start = { x: this.car.x, y: this.car.y }
       const end = {
