87bd03fe9c3d46e3ae31a93fb12454c99487a4eb
Mathias Johansson <hej@mathiasjohansson.se>
2023-08-03 14:44:24 +0200

    Detect crashes

--- car.js
+++ car.js
@@ -10,14 +10,56 @@ class Car {
     this.maxSpeed = 3
     this.friction = 0.05
     this.angle = 0
+    this.damaged = false
 
     this.sensor = new Sensor(this)
     this.controls = new Controls()
   }
 
   update(roadBoarders) {
-    this.#move()
+    if (!this.damaged) {
+      this.#move()
+      this.poligon = this.#createPolygon()
+      this.damaged = this.#assessDamage(roadBoarders)
+    }
     this.sensor.update(roadBoarders)
+  }
+
+  #assessDamage(roadBoarders) {
+    for (let i = 0; i < roadBoarders.length; i++) {
+      if (polysIntersect(this.#createPolygon, roadBoarders[i])) {
+        return true
+      }
+    }
+    return false
+  }
+
+  #createPolygon() {
+    const points = []
+    const rad = Math.hypot(this.width, this.height) / 2
+    const alpha = Math.atan2(this.width, this.height)
+    points.push({
+      // top right
+      x: this.x - Math.sin(this.angle - alpha) * rad,
+      y: this.y - Math.cos(this.angle - alpha) * rad,
+    })
+    points.push({
+      // top left
+      x: this.x - Math.sin(this.angle + alpha) * rad,
+      y: this.y - Math.cos(this.angle + alpha) * rad,
+    })
+    points.push({
+      // Bottom right
+      x: this.x - Math.sin(Math.pi + this.angle - alpha) * rad,
+      y: this.y - Math.cos(Math.pi + this.angle - alpha) * rad,
+    })
+    points.push({
+      // Bottom left
+      x: this.x - Math.sin(Math.pi + this.angle + alpha) * rad,
+      y: this.y - Math.cos(Math.pi + this.angle + alpha) * rad,
+    })
+
+    return points
   }
 
   #move() {
@@ -60,16 +102,16 @@ class Car {
   }
 
   draw(ctx) {
-    ctx.save()
-    ctx.translate(this.x, this.y)
-    ctx.rotate(-this.angle)
-
+    ctx.fillStyle = 'black'
+    if (this.damaged) {
+      ctx.fillStyle = 'gray'
+    }
     ctx.beginPath()
-    ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height)
+    ctx.moveTo(this.poligon[0].x, this.poligon[0].y)
+    for (let i = 1; i < this.poligon.length; i++) {
+      ctx.lineTo(this.poligon[i].x, this.poligon[i].y)
+    }
     ctx.fill()
-
-    ctx.restore()
-
     this.sensor.draw(ctx)
   }
 }
